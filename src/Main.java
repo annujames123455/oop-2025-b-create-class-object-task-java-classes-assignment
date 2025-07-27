e57ef8de2092d207f3329487f09268d24252d101
annujames <annujames2028@cs.sjcetpalai.ac.in>
2025-07-27 07:51:38 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
-     // TODO: Create a class called Student with name, rollNumber, and grade
-
-    // TODO: Create a method to display student details
-public class Main {
-      public static void main(String[] args) {
-        // TODO: Create a Student object and call the method to display details
-    }
-}
+// TODO: Create a class called Student with name, rollNumber, and grade // TODO: Create a method to display student details 
+class Student{ String name; int rollNumber; String grade; 
+     public Student(String name, int rollNumber, String grade){
+     this.name=name; 
+     this.rollNumber=rollNumber; 
+     this.grade=grade;
+     } public void displayDetails(){
+     System.out.println("Name : " +name); 
+     System.out.println("Roll No: " +rollNumber); 
+     System.out.println("Grade: " +grade);
+     }
+} public class Main{
+public static void main(String[] args){ 
+     Student student1 = new Student("Annu James", 25
+, "A+"); student1.displayDetails();
+ } }
